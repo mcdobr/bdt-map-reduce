b011cd66b1443a5b3beff8842123895b4bbebc9a
mcdobr <mircea.dobreanu@gmail.com>
2020-02-22 20:44:43 +0200

    Single threaded crawler

--- src/main/java/me/mircea/bdt/Application.java
+++ src/main/java/me/mircea/bdt/Application.java
@@ -1,10 +1,21 @@
 package me.mircea.bdt;
 
 import me.mircea.bdt.crawl.SequentialCrawler;
-import org.springframework.beans.factory.annotation.Autowired;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.URI;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -12,11 +23,29 @@ public class Application implements CommandLineRunner {
         SpringApplication.run(Application.class, args);
     }
 
-    @Autowired
-    private SequentialCrawler sequentialCrawler;
+    @Value("${crawler.filesystem.target.path}")
+    private String defaultFilesystemTargetPath;
+
+    @Value("${crawler.seeds}")
+    private String[] defaultSeeds;
+
+    @Value("${crawler.page.limit}")
+    private long defaultPageLimit;
 
     @Override
     public void run(String... args) throws Exception {
-        sequentialCrawler.crawl();
+        List<HttpUrl> seedUrls = Arrays.stream(defaultSeeds)
+                .map(HttpUrl::get)
+                .collect(Collectors.toUnmodifiableList());
+
+        SequentialCrawler sequentialCrawler = SequentialCrawler.builder()
+                .httpClient(new OkHttpClient())
+                .crawlFrontier(new ConcurrentLinkedQueue<>(seedUrls))
+                .visited(new LinkedHashSet<>())
+                .pageLimit(defaultPageLimit)
+                .fileSystemTargetPath(Path.of(URI.create(defaultFilesystemTargetPath)))
+                .build();
+
+        sequentialCrawler.start();
     }
 }
--- src/main/java/me/mircea/bdt/crawl/SequentialCrawler.java
+++ src/main/java/me/mircea/bdt/crawl/SequentialCrawler.java
@@ -1,7 +1,7 @@
 package me.mircea.bdt.crawl;
 
-import com.google.common.collect.ImmutableList;
-import lombok.RequiredArgsConstructor;
+import lombok.Builder;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -10,37 +10,64 @@ import okhttp3.Response;
 import okhttp3.ResponseBody;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Queue;
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Slf4j
-@Component
-@RequiredArgsConstructor
+@Builder
 public class SequentialCrawler {
-    private final Queue<HttpUrl> crawlFrontier;
+    @NonNull
+    private final ConcurrentLinkedQueue<HttpUrl> crawlFrontier;
+
+    @NonNull
     private final Set<HttpUrl> visited;
+
+    @NonNull
     private final OkHttpClient httpClient;
 
-//    @Value("${crawler.seeds}")
-//    private ImmutableList<String> seeds;
-    @Value("${crawler.page.limit}")
-    private long pageLimit;
-//    @Value("${crawler.filesystem.target.path}")
-//    private Path fileSystemTargetPath;
+    @NonNull
+    private final Path fileSystemTargetPath;
 
-    public void crawl() {
-        while (visited.size() < pageLimit) {
-            crawlOnePage();
+    @NonNull
+    private final UUID uuid = UUID.randomUUID();
+
+    private final long pageLimit;
+
+    public void start() {
+        log.info("Started crawl job {} with crawl frontier: {}", uuid, crawlFrontier);
+
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        ScheduledFuture<?> scheduledFuture = executorService.scheduleAtFixedRate(
+                this::crawl, 0, 1, TimeUnit.SECONDS);
+
+        try {
+            scheduledFuture.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
+
+        log.info("Finished crawl job {}", uuid);
     }
 
-    private void crawlOnePage() {
-        while (!crawlFrontier.isEmpty()) {
+    private void crawl() {
+        if (!crawlFrontier.isEmpty()) {
             HttpUrl url = crawlFrontier.remove();
 
             Request getRequest = new Request.Builder()
@@ -48,18 +75,58 @@ public class SequentialCrawler {
                     .build();
 
             try (Response response = httpClient.newCall(getRequest).execute()) {
-                ResponseBody responseBody = response.body();
+                var documentLinksPair = harvestResponse(url, response);
+                visited.add(url);
+                crawlFrontier.addAll(documentLinksPair.getValue());
 
-                Document htmlDocument = Jsoup.parse(responseBody.byteStream(), "UTF-8", url.toString());
-
-                htmlDocument.select("a[href]").stream()
-                        .map(anchor -> anchor.attr("abs:href"))
-                        .map(HttpUrl::get)
-                        .forEach(visited::add);
+                CompletableFuture.runAsync(() -> {
+                    try {
+                        persistResponseBody(url, documentLinksPair.getKey());
+                    } catch (IOException e) {
+                        log.warn("Could not persist response file for url {}", url);
+                    }
+                });
             } catch (IOException e) {
-                log.info("Could not download page {}", url.toString(), e);
+                log.warn("Could not download page {}", url.toString(), e);
             }
         }
     }
 
+    private Map.Entry<Document, Set<HttpUrl>> harvestResponse(HttpUrl url, Response response) throws IOException {
+        log.info("Parsing response from {}", url);
+        visited.add(url);
+
+        ResponseBody responseBody = response.body();
+
+        Document htmlDocument = Jsoup.parse(responseBody.byteStream(), "UTF-8", url.toString());
+
+        Set<HttpUrl> urls = htmlDocument.select("a[href]").stream()
+                .map(anchor -> anchor.attr("abs:href"))
+                .map(HttpUrl::parse)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+        log.trace("Found the following well formed absolute links: {}", urls);
+        return new AbstractMap.SimpleEntry<>(htmlDocument, urls);
+    }
+
+    private void persistResponseBody(HttpUrl url, Document document) throws IOException {
+        // todo: needs more work for directory and file naming
+        Path hostPath = fileSystemTargetPath.resolve(url.scheme())
+                .resolve(url.host());
+
+        Path resourcePath = hostPath;
+        for (String segment : url.pathSegments()) {
+            if (!segment.equals("")) {
+                resourcePath = resourcePath.resolve(segment);
+            } else {
+                resourcePath = resourcePath.resolve("index.html");
+            }
+        }
+//        if (url.pathSegments().isEmpty()) {
+//            resourcePath = resourcePath.resolve("index.html");
+//        }
+
+        Files.createDirectories(resourcePath.getParent());
+        Files.writeString(resourcePath, document.toString());
+    }
 }
