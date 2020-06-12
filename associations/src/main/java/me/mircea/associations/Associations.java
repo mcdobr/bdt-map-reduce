2e2925959c35459fd5102e06bf7eacaf54b30edf
mcdobr <mircea.dobreanu@gmail.com>
2020-06-12 09:08:51 +0300

    Add empty main class

--- associations/src/main/java/me/mircea/associations/Associations.java
+++ associations/src/main/java/me/mircea/associations/Associations.java
@@ -1,6 +1,9 @@
-package me.mircea;
+package me.mircea.associations;
+
+import org.apache.hadoop.conf.Configuration;
 
 public class Associations {
     public static void main(String[] args) {
+        Configuration configuration = new Configuration();
     }
 }
